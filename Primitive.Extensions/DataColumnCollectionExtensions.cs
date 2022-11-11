1bdd01b75a2d1f51487ded01fedff2b2ac10e83d
Veera Pallati <49051599+pallativ@users.noreply.github.com>
2022-11-11 05:43:27 +0530

    Add Data collection extensions. (#17)
    * Add Data collection extensions.
    
    * Adding data table & column collection extensions

---
+++ Primitive.Extensions/DataColumnCollectionExtensions.cs
@@ -0,0 +1,8 @@
+using System.Data;
+using System;
+using System.Collections;
+public static class DataColumnCollectionExtensions {
+    public static void Add(this DataColumnCollection dataColumnCollection, IDictionary<string, Type> columns){
+
+    }
+}
---
+++ Primitive.Extensions/DataTableExtensions.cs
@@ -0,0 +1,6 @@
+using System.Data;
+public static class DataTableExtensions {
+    public static void AddRow(this DataTable dataTable, object[] record){
+
+    }
+}
