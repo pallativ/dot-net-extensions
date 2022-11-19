7554c169349f2b6145cd3d487b6f6dbfef47fca9
Veera Pallati <49051599+pallativ@users.noreply.github.com>
2022-11-19 17:46:39 +0530

    Adding date time extensions (#35)

---
+++ Primitive.Extension.Tests/DateTimeExtensionTests.cs
@@ -0,0 +1,38 @@
+using Primitive.Extensions;
+
+namespace Primitive.Extension.Tests;
+
+public class DateTimeExtensionTests
+{
+    [Theory]
+    [InlineData("10/11/2020", "dd/MM/yyyy")]
+    [InlineData("10-11-2020", "dd-MM-yyyy")]
+    [InlineData("11-10-2020", "MM-dd-yyyy")]
+    public void VerifyToDate(string dateString, string format)
+    {
+        var resultDate = dateString.ToDateTime(format);
+        Assert.Equal(new DateTime(2020, 11, 10), resultDate);
+    }
+
+    [Fact]
+    public void VerifyToEpochDate()
+    {
+        var expectedDate = new DateTime(2020, 10, 10);
+        var epochSeconds = new DateTime(2020, 10, 10).ToEpochTime();
+        var actualDate = DateTimeOffset.FromUnixTimeSeconds((long)epochSeconds);
+        Assert.Equal(expectedDate, actualDate);
+    }
+
+    [Theory]
+    [InlineData("10/11/2020", "dd/MM/yyyy", true)]
+    [InlineData("10-11-2020", "dd-MM-yyyy", true)]
+    [InlineData("10-11-2020", "MM-dd-yyyy", true)]
+    [InlineData("13-11-2020", "MM-dd-yyyy", false)]
+    [InlineData("13-32-2020", "MM-dd-yyyy", false)]
+    public void VerifyIsValidDateTime(string date, string format, bool expectedResult)
+    {
+        var result = date.IsValid(out _, format);
+        Assert.Equal(expectedResult, result);
+    }
+
+}
---
+++ Primitive.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace Primitive.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public const string DateTimeFormat = "dd-MM-yyyy HH:mm:ss";
+
+        /// <summary>
+        /// Converts the given date time to epoch date time.
+        /// </summary>
+        /// <param name="date">given input date.</param>
+        /// <returns></returns>
+        public static double ToEpochTime(this DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan diff = date.ToUniversalTime() - origin;
+            return diff.TotalSeconds;
+        }
+
+        /// <summary>
+        /// Converts to dateTime using the given datetime format. Default Format: dd-MM-yyyy HH:mm:ss
+        /// </summary>
+        /// <param name="dateTimeString"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this string dateTimeString, string format = DateTimeFormat)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            var dateTime = DateTime.ParseExact(dateTimeString, format, provider);
+            return dateTime;
+        }
+
+        /// <summary>
+        /// Validates the given date string and outputs the result date if it is valid.
+        /// </summary>
+        /// <param name="dateTimeString">Input datetime string to be validated.</param>
+        /// <param name="dateTime">Output parameter</param>
+        /// <param name="format">Format of the date time string to be validated against</param>
+        /// <returns></returns>
+        public static bool IsValid(this string dateTimeString, out DateTime dateTime, string format = DateTimeFormat)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            return DateTime.TryParseExact(dateTimeString, format, provider,
+                DateTimeStyles.AdjustToUniversal, out dateTime);
+        }
+    }
+}
