0ed30d65bbb512a684d6556ca542787a7500afe4
Pallati Veera <pkondalu@gmail.com>
2022-11-22 19:49:09 +0530

    Fixing style cop issues.

--- Primitive.Extensions/Builders/BaseEntityCriteriaMapper.cs
+++ Primitive.Extensions/Builders/BaseEntityCriteriaMapper.cs
@@ -1,18 +1,24 @@
-﻿namespace Primitive.Extensions.Builders;
+﻿// Copyright (c) VajraTechMinds.com. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Primitive.Extensions.Builders;
 
 public abstract class BaseEntityCriteriaMapper : IEntityCriteriaMapper
 {
     protected abstract IDictionary<string, string> FieldsMapping { get; set; }
+
     public string GetEntityFieldPath(string fieldName)
     {
         if (IsExists(fieldName))
             return GetMappedFieldName(fieldName);
         throw new InvalidEntityCriteriaFieldException();
     }
+
     public bool IsExists(string fieldName)
     {
         return FieldsMapping.ToList().Any(t => t.Key.ToUpper().Equals(fieldName.ToUpper()));
     }
+
     private string GetMappedFieldName(string fieldName)
     {
         var keyValuePair = FieldsMapping.ToList().First(t => t.Key.ToUpper().Equals(fieldName.ToUpper()));
